dc43e6ca9f83b62c3ffe1873d9834740b9c75fb4
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-10-04 17:46:58 -0300

    Primeiro commit - Criação do Product Service e seu teste de integração

---
+++ product-service/src/main/java/com/rr/microservice/product/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.rr.microservice.product.service;
+
+import com.rr.microservice.product.dto.ProductRequest;
+import com.rr.microservice.product.dto.ProductResponse;
+import com.rr.microservice.product.model.Product;
+import com.rr.microservice.product.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created successfully");
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProduct() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
