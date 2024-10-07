3607ded0bb7414fb34440447665c6d3cab591e13
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-10-06 21:54:12 -0300

    Criação do Inventory Service e seu teste de integração

---
+++ inventory-service/src/main/java/com/rr/microservice/inventory/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.rr.microservice.inventory.controller;
+
+import com.rr.microservice.inventory.service.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
---
+++ inventory-service/src/main/java/com/rr/microservice/inventory/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.rr.microservice.inventory.repository;
+
+import com.rr.microservice.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
