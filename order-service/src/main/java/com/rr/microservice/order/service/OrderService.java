fdb540c56a41a2a5d11482e3e28b812a74bfa82b
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-10-07 15:41:19 -0300

    Comunicação entre Order Service e Inventory Service utilizando OpenFeign e utilização do Wiremock nos testes de integração

--- order-service/src/main/java/com/rr/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/rr/microservice/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.rr.microservice.order.service;
 
+import com.rr.microservice.order.client.InventoryClient;
 import com.rr.microservice.order.dto.OrderRequest;
 import com.rr.microservice.order.model.Order;
 import com.rr.microservice.order.repository.OrderRepository;
@@ -11,18 +12,26 @@ import java.util.UUID;
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
         this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
     }
 
     @Transactional
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
